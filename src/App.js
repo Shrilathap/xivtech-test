f6f1cd9a557cbf716509ecd53f6b1f81c0e3bf5c
shrilathap <shrilatha.poojary.56@gmail.com>
2023-05-12 17:28:51 +0530

    data store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import MediaControlCard from './childcomp';
+import MediaControlCard from './cardcomp';
 const App=(props)=>{
   const arr=[{id:1,title:'AI + RPA is what we do',description:'Future-Proof your business. Drive efficiency, profitability and deliver on customer experience',img:'https://www.xivtech.io./rp2.jpg'},{id:2,title:'Make Bolder Choices',description:'Digital focused strategies to realize market-changing ideas',img:'https://www.xivtech.io./p1.png'},{id:3,title:'Innovate with Speed',description:'Create higher quality software, deliver on customer expectations and business goals',img:'https://www.xivtech.io./p2.jpg'},{id:4,title:'Embrace Cloud',description:'With Cloud-First accelerate innovation and optimize performance',img:'https://www.xivtech.io./p3.jpg'}]
   return (
